dd3e01670ba6bcf6a23f436d735d3e0b42f213c6
harivilasp <harivilasp@gmail.com>
2023-04-01 18:27:22 -0400

    updated tweet like values

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -4,14 +4,14 @@ export default [
      "handle": "@spacex", "time": "2h", "avatar": "spacex.png",
      "title": "SpaceX's Mission",
      "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-     "liked": true, "like": 2345, "reply": 123, "retuit": 432
+     "liked": true, "like": 3343, "reply": 43, "retuit": 432
     },
     {
      "_id": "234", "topic": "Traffic", "username": "The Boring Company",
      "handle": "@boringcompany", "time": "2h", "avatar": "boring2.jpg",
      "title": "The Boring Company fixing traffic",
      "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "like": 2345, "reply": 321, "retuit": 432
+     "liked": true, "like": 2345, "reply": 321, "retuit": 233
     },
     {
      "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
